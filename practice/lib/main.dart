5bde786b67ce39988d30d80e83f99e5f14c98373
felix <2319979647@qq.com>
2019-07-30 16:34:19 +0800

    添加图形绘制

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -1,8 +1,110 @@
 import 'package:flutter/material.dart';
 
 // void main() => runApp(MyApp()); // 基本的首页（常规）
+// void main() => runApp(FadeTest()); // 渐变动画
+// void main() => runApp(DemoApp());
+// void main() => runApp(MaterialApp(home: DemoApp()));
+void main() => runApp(Painter());
 
-void main() => runApp(FadeTest());
+//  绘制原型和正方形
+// Flutter
+// 在Flutter中，你可以使用 CustomPaint 和 CustomPainter 类去绘制到画布。
+// 以下示例显示如何使用CustomPaint widget在绘制阶段绘制。 它实现了抽象类CustomPainter，并将其传递给CustomPaint的painter属性。 CustomPaint子类必须实现paint和shouldRepaint方法：
+
+class Painter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '绘制正放心和长方形',
+      theme: ThemeData.fallback(),
+      home: _MyCanvas(),
+    );
+  }
+}
+
+class CustomCanvasPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint();
+    paint.color = Colors.amber;
+    canvas.drawCircle(Offset(100.0 ,200.0), 40.0, paint);
+
+    Paint paintRect = Paint();
+    paintRect.color = Colors.lightBlue;
+    Rect rect = Rect.fromPoints(Offset(150.0, 300.0), Offset(300.0, 400.0));
+    canvas.drawRect(rect, paintRect);
+  }
+
+  bool shouldRepaint(CustomCanvasPainter oldDelegate) => false;
+  bool shouldRebuildSemantics(CustomCanvasPainter oldDelegate) => false;
+}
+
+class _MyCanvas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomPaint(
+          painter: CustomCanvasPainter(),
+      ),
+    );
+  }
+}
+
+
+// class _MyCanvas extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: CustomPaint(
+//         painter: CustomCanvasPainter(),
+//       ),
+//     );
+//   }
+// }
+
+//  前面的渲染例子 , 自定义画布的实现方式
+class DemoApp extends StatelessWidget {
+    Widget build(BuildContext context) => Scaffold(body: Signature());
+}
+
+class Signature extends StatefulWidget {
+  // SignatureState({Key key}) : super(key: key);
+  SignatureState createState() => SignatureState();
+}
+
+class SignatureState extends State<Signature> {
+  List<Offset> _points = <Offset>[];
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanUpdate: (DragUpdateDetails details) {
+        setState(() {
+          RenderBox referenceBox = context.findRenderObject();
+          Offset localPosition =
+          referenceBox.globalToLocal(details.globalPosition);
+          _points = List.from(_points)..add(localPosition);
+        });
+      },
+      onPanEnd: (DragEndDetails details) => _points.add(null),
+      child: CustomPaint(painter: SignaturePainter(_points), size: Size.infinite),
+    );
+  }
+}
+
+class SignaturePainter extends CustomPainter {
+  SignaturePainter(this.points);
+  final List<Offset> points;
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.black
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5.0;
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null)
+        canvas.drawLine(points[i], points[i + 1], paint);
+    }
+  }
+  bool shouldRepaint(SignaturePainter other) => other.points != points;
+}
 
 // 这个是关于点击，有动画效果的内容， 将animation添加到controller里面去； 
 class FadeTest extends StatelessWidget {
